cfc2136fa5f7ba6ddbbe7009f9dd56b527f9f223
Adam Azad <adam@adamazad.com>
2022-05-17 09:14:09 +0100

    feat(#1): make queries case insensitive

--- src/modules/twitter/controllers/twitter.controllers.ts
+++ src/modules/twitter/controllers/twitter.controllers.ts
@@ -1,7 +1,8 @@
 import { captureException } from '@sentry/node';
-import dayjs from 'dayjs';
 import dayjsUtcPlugin from 'dayjs/plugin/utc';
 import Boom from '@hapi/boom';
+import debug from 'debug';
+import dayjs from 'dayjs';
 
 dayjs.extend(dayjsUtcPlugin);
 
@@ -11,6 +12,10 @@ import { TwitterService } from '../services/twitter.service';
 import { TwitterUserModel } from '../models/TwitterUser.model';
 import { IRequest } from 'src/modules/shared/interfaces/request.interface';
 
+const debugTwitterController = debug(
+  'controllers:twitter:getTwitterProfileByUsername'
+);
+
 /**
  * Fetches Twitter information for a given username
  */
@@ -19,14 +24,17 @@ export async function getTwitterProfileByUsername(req: IRequest) {
     const { query } = req;
     // Services
     const twitterService = new TwitterService(TWITTER_API_V2_BEARER_TOKEN);
+
     // Fetch from mongo
     const twitterUserFromMongo = await TwitterUserModel.findOne({
-      username: query.username,
-    });
+      username: { $regex: new RegExp(query.username, 'i') },
+    }).exec();
 
+    debugTwitterController(twitterUserFromMongo?.toJSON());
+
+    // If results from Mongo are less than 20 minutes old, return them
     if (
       twitterUserFromMongo != null &&
-      twitterUserFromMongo.username != null &&
       dayjs().utc().diff(twitterUserFromMongo.updatedAt) < 1000 * 60 * 20 // 20 minutes
     ) {
       return {
@@ -41,13 +49,13 @@ export async function getTwitterProfileByUsername(req: IRequest) {
       query.username
     );
 
-    console.log({ twitterUserFromAPI });
+    debugTwitterController(twitterUserFromAPI);
 
     if (!twitterUserFromAPI) {
       throw new Error('Twitter user not found');
     }
 
-    // Update the local cache
+    // Save to database
     await TwitterUserModel.updateOne(
       { username: twitterUserFromAPI.username },
       {
@@ -68,6 +76,7 @@ export async function getTwitterProfileByUsername(req: IRequest) {
       }
     );
 
+    // Fetch the user again from the database
     const twitterUserFromMongoAfterUpdate = await TwitterUserModel.findOne({
       username: query.username,
     });
